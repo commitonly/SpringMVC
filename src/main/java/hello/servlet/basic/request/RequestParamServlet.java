0c616fcb276f39b58da8c9161b475de792ab0569
taemin <myloopmy@gmail.com>
2023-01-12 00:54:13 +0900

    [전체 파라미터 조회] [단일 파라미터 조회] [이름이 같은 복수 파라미터 조회]

--- src/main/java/hello/servlet/basic/request/RequestParamServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -31,5 +31,21 @@ public class RequestParamServlet extends HttpServlet {
 
         request.getHeaderNames();
 
+        System.out.println("[단일 파라미터 조회]");
+        String username = request.getParameter("username");
+        String age = request.getParameter("age");
+
+        System.out.println("username = " + username );
+        System.out.println("age = " + age);
+
+        System.out.println("[이름이 같은 복수 파라미터 조회");
+        String[] usernames = request.getParameterValues("username");
+        for (String name : usernames) {
+            System.out.println("username = " + name);
+            
+        }
+        
+
+
     }
 }
