7de122d120566d3f428ec24fbfebe5fac44e7abc
taemin <myloopmy@gmail.com>
2023-01-12 15:22:14 +0900

    json 파싱 테스트

--- src/main/java/hello/servlet/basic/request/RequestBodyStringServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestBodyStringServlet.java
@@ -1,5 +1,7 @@
 package hello.servlet.basic.request;
 
+import org.springframework.util.StreamUtils;
+
 import javax.servlet.ServletException;
 import javax.servlet.ServletInputStream;
 import javax.servlet.annotation.WebServlet;
@@ -7,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 
 @WebServlet(name = "RequestBodyStringServlet", urlPatterns = "/request-body-string")
@@ -15,5 +18,10 @@ public class RequestBodyStringServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         ServletInputStream inputStream = request.getInputStream();
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        System.out.println("messageBody = " + messageBody);
+
+        response.getWriter().write("ok");
     }
 }
