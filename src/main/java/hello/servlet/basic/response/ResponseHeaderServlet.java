1494fda19948d2c87b211f8fd68c5fad48cc1b6c
taemin <myloopmy@gmail.com>
2023-01-12 16:42:54 +0900

    response 다양한 방식 시도

--- src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
+++ src/main/java/hello/servlet/basic/response/ResponseHeaderServlet.java
@@ -6,12 +6,45 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "responseHeaderServlet", urlPatterns = "/response-header")
 public class ResponseHeaderServlet extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+        // [status-line]
         response.setStatus(HttpServletResponse.SC_OK);
+
+        // [response-headers]
+        response.setHeader("Content-Type", "text/plain;charset=utf-8");
+        response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        response.setHeader("Pragma", "no-cache");
+        response.setHeader("my-header", "hello");
+
+
+
+        // [Header 편의 메서드]
+        content(response);
+
+        PrintWriter writer = response.getWriter();
+        writer.println("ok");
+
     }
+    private void content(HttpServletResponse response) {
+
+        //Content-Type: text/plain;charset=utf-8
+        //Content-Length: 2
+
+        //response.setHeader("Content-Type", "text/plain;charset=utf-8");
+
+        // 위 방식을 더 간단하게 사용할 수 있는 코드는 아래 2줄이다
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+
+        // response.setContentLength(2); //(생략시 자동 생성)
+    }
+
 }
